0d2e211c0b031a2b6d8f549dace9482e0a293501
mohamadmahditayari <mohamadmahditayari20021381@gmail.com>
2026-08-13 04:15:01 +0330

    Fix env variables

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://btxhaeslqhxcorvgicyl.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_pECz6hGQ_znXoAHs-mAtZA_5Oh668QZ","NEXT_PUBLIC_SUPABASE_STORAGE_BUCKET":"product-images"};
-export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://btxhaeslqhxcorvgicyl.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_pECz6hGQ_znXoAHs-mAtZA_5Oh668QZ","NEXT_PUBLIC_SUPABASE_STORAGE_BUCKET":"product-images"};
-export const test = {};
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Y5KWuoZwpFtGlSn5a6MAx2X3I+m06fIvLdEcncElklQ=\"\n}"
